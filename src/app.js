5ba2bd89b3bfae1f1defef0667b8a83a3d8d7ac7
DaQshin <dakshin.csml@gmail.com>
2026-08-05 18:26:13 +0000

    feat: add  Swagger UI

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const swaggerui = require('swagger-ui-express');
+const openapi = require('../docs/openapi.json');
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use('/docs', swaggerui.serve, swaggerui.setup(openapi));
 
 var tasks = [
   {
@@ -25,7 +29,7 @@ var tasks = [
 
 app.get('/', (req, res) => {
   res
-    .staus(200)
+    .status(200)
     .json({ name: 'Task API', version: '1.0', endpoints: ['/tasks'] });
 });
 
@@ -36,12 +40,45 @@ app.get('/tasks', (req, res) => {
 });
 
 app.get('/tasks/:id', (req, res) => {
-  if (id < tasks.length)
-    res.status(200).json({
-      task: tasks[id],
-    });
+  const id = parseInt(req.params.id, 10);
+  if (id >= tasks.length)
+    res.status(404).json({ error: `Task ${id} not found` });
+  const task = tasks.find((t) => t.id === id);
+  res.status(200).json({
+    task,
+  });
+});
 
-  res.status(404).json({ error: `Task ${id} not found` });
+app.post('/tasks', (req, res) => {
+  const title = req.body.title;
+  if (title === '') res.status(400).json({});
+  tasks.push({ id: tasks.length, title, done: false });
+  res.status(201).json({ message: 'task added successfully' });
+});
+
+app.put('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (id >= 0 && id >= tasks.length) res.status(404).json({});
+  const title = req.body.title;
+  const done = req.body.done;
+
+  if (title === undefined || done === undefined) res.status(400).json({});
+
+  if (title != undefined) {
+    tasks[id].title = title;
+  }
+  if (done != undefined) {
+    tasks[id].done = done;
+  }
+
+  res.status(202).json({});
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (id >= 0 && id >= tasks.length) res.status(404).json({});
+  tasks.splice(id, 1);
+  res.status(204).json({});
 });
 
 app.get('/health', (req, res) => {
