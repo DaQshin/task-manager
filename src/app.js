f086fc663bff94e76be460dc4198e2b96eac9ec6
DaQshin <dakshin.csml@gmail.com>
2026-08-05 17:37:36 +0000

    feat: read endpoints with 404

--- src/app.js
+++ src/app.js
@@ -5,10 +5,43 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+var tasks = [
+  {
+    id: 1,
+    title: 'Learn Express.js',
+    done: false,
+  },
+  {
+    id: 2,
+    title: 'Build Todo API',
+    done: true,
+  },
+  {
+    id: 3,
+    title: 'Write Swagger Documentation',
+    done: false,
+  },
+];
+
 app.get('/', (req, res) => {
   res
     .staus(200)
     .json({ name: 'Task API', version: '1.0', endpoints: ['/tasks'] });
+});
+
+app.get('/tasks', (req, res) => {
+  res.status(200).json({
+    tasks,
+  });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  if (id < tasks.length)
+    res.status(200).json({
+      task: tasks[id],
+    });
+
+  res.status(404).json({ error: `Task ${id} not found` });
 });
 
 app.get('/health', (req, res) => {
